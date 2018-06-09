5c836821fb28300ad0d5b5358f43673c606b865d
hexiaoming <17710146600@163.com>
2018-06-09 12:15:28 +0800

    范德萨

--- src/builder/A.java
+++ src/builder/A.java
@@ -2,12 +2,12 @@ package builder;
 /**
  * 
 *类描述：三种放法都会导致栈内存溢出 //栈溢出抛出StackOverflowError错误
-*@author: Admini
 *@date: 日期:2018年5月26日 时间:上午8:39:05
+@admin
 *@version 1.0
  */
 public class A {
-		A a_ = new A();
+		A a_a = new A();
 	public A(){
 		A _a = new A();
 		A A_B =new A();
